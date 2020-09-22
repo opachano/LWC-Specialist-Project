441a8e8cade2890f4fbdd125f1cfacdf76a3f29a
Oscar Pachano <opachano@gmail.com>
2020-09-22 12:01:32 -0400

    Added value for message channel

--- force-app/main/default/lwc/boatMap/boatMap.js
+++ force-app/main/default/lwc/boatMap/boatMap.js
@@ -1,5 +1,11 @@
 // import BOATMC from the message channel
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import {
+  subscribe,
+  unsubscribe,
+  APPLICATION_SCOPE,
+  MessageContext
+} from 'lightning/messageService'
 import BOATMC from '@salesforce/messageChannels/BoatMessageChannel__c';
 // Declare the const LONGITUDE_FIELD for the boat's Longitude__s
 // Declare the const LATITUDE_FIELD for the boat's Latitude
