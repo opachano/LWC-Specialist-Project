963245ea871989d4349f0663fd6514dfb7f86e17
Oscar Pachano <opachano@gmail.com>
2020-09-24 12:00:07 -0400

    Done with boatAddReviewForm component

--- force-app/main/default/lwc/boatAddReviewForm/boatAddReviewForm.js
+++ force-app/main/default/lwc/boatAddReviewForm/boatAddReviewForm.js
@@ -1,5 +1,6 @@
-import { LightningElement } from 'lwc';
 // imports
+import { LightningElement, track, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 // import BOAT_REVIEW_OBJECT from schema - BoatReview__c
 // import NAME_FIELD from schema - BoatReview__c.Name
 // import COMMENT_FIELD from schema - BoatReview__c.Comment__c
@@ -12,6 +13,7 @@ const SUCCESS_VARIANT = "success";
 
 export default class BoatAddReviewForm extends LightningElement {
   // Private
+  @track
   boatId;
   rating;
   boatReviewObject = BOAT_REVIEW_OBJECT;
@@ -21,24 +23,39 @@ export default class BoatAddReviewForm extends LightningElement {
   labelRating  = 'Rating';
   
   // Public Getter and Setter to allow for logic to run on recordId change
-  get recordId() { }
+  @api
+  get recordId() { 
+    return this.boatId;
+  }
   set recordId(value) {
     //sets boatId attribute
+    // this.setAttribute('boatId', value);
     //sets boatId assignment
+    this.boatId = value;
   }
   
   // Gets user rating input from stars component
-  handleRatingChanged(event) { }
+  handleRatingChanged(event) {
+    this.rating = event.detail.rating;
+   }
   
   // Custom submission handler to properly set Rating
   // This function must prevent the anchor element from navigating to a URL.
   // form to be submitted: lightning-record-edit-form
-  handleSubmit(event) { }
+  handleSubmit(event) { 
+    event.preventDefault();
+    const fields = event.detail.fields;
+    fields.Rating__c = this.rating;
+    fields.Boat__c = this.boatId;
+    this.template.querySelector('lightning-record-edit-form').submit(fields);
+  }
   
   // Shows a toast message once form is submitted successfully
   // Dispatches event when a review is created
   handleSuccess() {
     // TODO: dispatch the custom event and show the success message
+    const reviewEvent = new CustomEvent('createreview');
+    this.dispatchEvent(reviewEvent);
     this.dispatchEvent(
       new ShowToastEvent({
           title: SUCCESS_TITLE,
@@ -50,5 +67,14 @@ export default class BoatAddReviewForm extends LightningElement {
   
   // Clears form data upon submission
   // TODO: it must reset each lightning-input-field
-  handleReset() { }
+  handleReset() {
+    const inputFields = this.template.querySelectorAll(
+      'lightning-input-field'
+    );
+    if (inputFields) {
+        inputFields.forEach(field => {
+          field.reset();
+      });
+    }
+  }
 }
